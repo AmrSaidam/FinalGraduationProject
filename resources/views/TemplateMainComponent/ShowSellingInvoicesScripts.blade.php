baa38f1b36788c46381d4be06f7e048bcc6bbd9d
Shadow <a.amer.2008@hotmail.com>
2016-06-20 13:11:19 +0300

    SellingInvoice is Done now

--- resources/views/TemplateMainComponent/ShowSellingInvoicesScripts.blade.php
+++ resources/views/TemplateMainComponent/ShowSellingInvoicesScripts.blade.php
@@ -3,7 +3,7 @@
 <script src="{{URL::asset('bootstraps/vendor/slimScroll/jquery.slimscroll.min.js')}}"></script>
 <script src="{{URL::asset('bootstraps/vendor/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 <script src="{{URL::asset('bootstraps/vendor/metisMenu/dist/metisMenu.min.js')}}"></script>
-<script src="{{URL::asset('bootstraps/}vendor/iCheck/icheck.min.js')}}"></script>
+<script src="{{URL::asset('bootstraps/vendor/iCheck/icheck.min.js')}}"></script>
 <script src="{{URL::asset('bootstraps/vendor/sparkline/index.js')}}"></script>
 <script src="{{URL::asset('bootstraps/vendor/moment/moment.js')}}"></script>
 <script src="{{URL::asset('bootstraps/vendor/select2-3.5.2/select2.min.js')}}"></script>
